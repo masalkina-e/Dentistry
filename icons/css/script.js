0eade3afdbe53993b1a4ace7a4105295e78ebe62
masalkina-e <katyamasalkina123@yandex.ru>
2022-11-07 18:20:51 +0500

    Добавлен файл JS, сделано мобильное меню

---
+++ icons/css/script.js
@@ -0,0 +1,16 @@
+<script>
+    let isOpen = false
+
+    function OpenMenu() {
+    const mobilMenu = document.getElementById("myMobilMenu") 
+    mobilMenu.style.display = "flex"
+
+    if (isOpen) {
+        mobilMenu.style.display = "none"
+        isOpen = false
+        } else {
+        mobilMenu.style.display = "flex"
+        isOpen = true
+        }
+    }
+</script>
