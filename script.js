20713b09029bab58d239106aa43f46e2df300fdc
masalkina-e <katyamasalkina123@yandex.ru>
2022-11-07 18:41:35 +0500

    Исправлена ошибка в ссылке на JS

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-<script>
-    let isOpen = false
 
-    function OpenMenu() {
+let isOpen = false
+        
+function OpenMenu() {
     const mobilMenu = document.getElementById("myMobilMenu") 
     mobilMenu.style.display = "flex"
-
+        
     if (isOpen) {
         mobilMenu.style.display = "none"
         isOpen = false
-        } else {
+    } else {
         mobilMenu.style.display = "flex"
         isOpen = true
-        }
     }
-</script>
+    }
