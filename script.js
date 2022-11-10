ded83d4ca23de27591d94bcbf85534a77b746485
masalkina-e <katyamasalkina123@yandex.ru>
2022-11-10 20:35:16 +0500

    При закрытии моб.меню добавлена иконка close

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ let isOpen = false
         
 function OpenMenu() {
     const mobilMenu = document.getElementById("myMobilMenu") 
-    mobilMenu.style.display = "flex"
+    const mobilIconBar = document.getElementById("myIconBar")
+    const mobilIconClose = document.getElementById("myIconClose")
         
     if (isOpen) {
         mobilMenu.style.display = "none"
+        mobilIconBar.style.display = "flex"
+        mobilIconClose.style.display = "none"
         isOpen = false
     } else {
         mobilMenu.style.display = "flex"
+        mobilIconClose.style.display = "flex"
+        mobilIconBar.style.display = "none"
         isOpen = true
     }
 }
+
+
